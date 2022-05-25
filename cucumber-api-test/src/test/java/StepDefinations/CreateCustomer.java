09a539e695a16afe1d318c9cb39eaa1ff5c2b54f
Jai Sharma <jsharma@pbs.org>
2022-05-25 13:40:44 -0400

    Implementation of JSON-Server middleware. support for happy and unhappy paths.

--- cucumber-api-test/src/test/java/StepDefinations/CreateCustomer.java
+++ cucumber-api-test/src/test/java/StepDefinations/CreateCustomer.java
@@ -1,3 +1,6 @@
+//json-server --routes ./src/test/resources/mockdata/routes.json -p 8081 ./src/test/resources/mockdata/customers-db.json --middlewares ./src/test/resources/middleware/validator.js
+//json-server --routes ./src/test/resources/mockdata/routes.json -p 8082 ./src/test/resources/mockdata/products-db.json --middlewares ./src/test/resources/middleware/validator.js
+//json-server --routes ./src/test/resources/mockdata/routes.json -p 8083 ./src/test/resources/mockdata/orders-db.json --middlewares ./src/test/resources/middleware/validator.js
 package StepDefinations;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -8,43 +11,60 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import java.io.File;
-
-
 public class CreateCustomer {
-
-
     RequestSpecification request;
     Response response;
 
-    @Given("^Setup Base API URL:$")
+    @Given("^Setup below base api url:$")
     public void setupbaseurl(String baseurl) {
-        System.out.println(baseurl);
         RestAssured.baseURI = baseurl;
         request = RestAssured.given().header("Content-Type", "application/json");
     }
 
-    @When("^I submit Scenario (.*) with payload (.*) to API endpoint (.*)$")
+    @When("^I submit scenario (.*) with payload (.*) to api endpoint (.*)$")
     public void iSubmitRequestToCreateCustomerWithPayload(String scenario, String payload, String endpoint) {
-       response = request.body(payload).post(endpoint).then().extract().response();
+        response = request.body(payload).post(endpoint).then().extract().response();
     }
 
-    @Then("^I should get Response Status Code as (.*)$")
+    @Then("^I should get response status code as (.*)$")
     public void iShouldGetresponseStatusCode(int status) {
         Assert.assertEquals(status, response.getStatusCode());
-
     }
 
-    @And("^I should get Response with new Customer ID$")
+    @And("^I should get response with new customer id$")
     public void iShouldGetReponseWithNewCustomerId() {
         int id = response.getBody().path("id");
         Assert.assertNotNull(id);
     }
 
-    @And("^I should get Response with value First Name as (.*) and Last Name as (.*)$")
+    @And("^I should get response with value first name as (.*) and last name as (.*)$")
     public void iShouldGetresponseWithValueFirstNameandLastName(String fname, String lname) {
         String firstname = response.getBody().path("fname");
         String lastname = response.getBody().path("lname");
         Assert.assertEquals(fname, firstname);
         Assert.assertEquals(lname, lastname);
+    }
+
+    //validate create customers scenarios for invalid field's value
+
+    @Given("^Setup base url to validate fields value:$")
+    public void setupbaseUrlForInvalid(String baseurl) {
+        RestAssured.baseURI = baseurl;
+        request = RestAssured.given().header("Content-Type", "application/json");
+    }
+    @When("^I submit and validate scenario (.*) with payload (.*) to api endpoint (.*)$")
+    public void iSubmitRequestToCreateCustomerWithPayloadForValidateFields(String scenario, String payload, String endpoint) {
+        response = request.body(payload).post(endpoint).then().extract().response();
+    }
+
+    @Then("^I should get response status code for error as (.*)$")
+    public void iShouldGetresponseStatusCodeForValidateFields(int status) {
+        Assert.assertEquals(status, response.getStatusCode());
+    }
+
+    @And("^I should get response with expected error as (.*)$")
+    public void iShouldGetReponseForErrorMessage(String expectedError) {
+        String actualError = response.getBody().path("error");
+        Assert.assertEquals(expectedError, actualError);
     }
 }
--- cucumber-api-test/src/test/java/StepDefinations/CreateProducts.java
+++ cucumber-api-test/src/test/java/StepDefinations/CreateProducts.java
@@ -12,22 +12,21 @@ import org.junit.Assert;
 
 public class CreateProducts {
 
-
     RequestSpecification request;
     Response response;
 
-    @Given("^Setup Product API URL:$")
+    @Given("^Setup base url to validate happy scenarios:$")
     public void setupBaseUrlforProduct(String baseurl) {
         RestAssured.baseURI = baseurl;
         request = RestAssured.given().header("Content-Type", "application/json");
     }
 
-    @When("^I submit product (.*) with product payload (.*) to api endpoint (.*)$")
-    public void iSubmitRequestToCreateProductsWithPayload(String scenario, String payload, String endpoint) {
+    @When("^I submit payload (.*) to api endpoint (.*) for product$")
+    public void iSubmitRequestToCreateProductsWithPayload(String payload, String endpoint) {
         response = request.body(payload).post(endpoint).then().extract().response();
     }
 
-    @Then("^I should get product response status code as (.*) for product$")
+    @Then("^I should get product response status code as (.*)$")
     public void iShouldGetresponseStatusCodeForProduct(int status) {
         Assert.assertEquals(status, response.getStatusCode());
     }
@@ -35,16 +34,60 @@ public class CreateProducts {
     @And("^I should get response with new product id$")
     public void iShouldGetReponseWithNewProductId() {
         int id = response.getBody().path("id");
-        Assert.assertNotNull(id)
-        ;
+        Assert.assertNotNull(id);
     }
 
-    @And("^I should get response with value product name as (.*) and price as (.*)$")
-    public void iShouldGetresponseWithValueProducNameandPrice(String pname, String price) {
-        String prodcintName = response.getBody().path("pname");
-        String productPrice = response.getBody().path("price");
-        Assert.assertEquals(pname, prodcintName);
-        Assert.assertEquals(price, productPrice);
+    @And("^I should get category name as (.*) in response$")
+    public void iShouldGetresponseWithValueCategoryName(String eCategoryName) {
+        String aCategoryName = response.getBody().path("category");
+        Assert.assertEquals(eCategoryName, aCategoryName);
+    }
+
+    @And("^I should get product name as (.*) in response$")
+    public void iShouldGetresponseWithValueProductName(String eProductName) {
+        String aProductName = response.getBody().path("pname");
+        Assert.assertEquals(eProductName, aProductName);
+    }
+
+    @And("^I should get price as (.*) in response$")
+    public void iShouldGetresponseWithPriceValue(String ePrice) {
+        String aPrice = response.getBody().path("price");
+        Assert.assertEquals(ePrice, aPrice);
+    }
+
+    @And("^I should get company name as (.*) in response$")
+    public void iShouldGetresponseWithCompanyName(String eCompanyName) {
+        String aCompanyName = response.getBody().path("company");
+        Assert.assertEquals(eCompanyName, aCompanyName);
+    }
+
+    @And("^I should get price in as (.*) in response$")
+    public void iShouldGetresponseWithPriceIn(String ePriceIn) {
+        String aPriceIn = response.getBody().path("pricein");
+        Assert.assertEquals(ePriceIn, aPriceIn);
+    }
+
+    //validate create product scenarios for unhappy path
+
+    @Given("^Setup base url to validate unhappy path scenarios:$")
+    public void setupbaseUrlForUhappyPath(String baseurl) {
+        RestAssured.baseURI = baseurl;
+        request = RestAssured.given().header("Content-Type", "application/json");
+    }
+    @When("^I submit unhappy scenario payload (.*) to api endpoint (.*)$")
+    public void iSubmitRequestToCreateProductWithPayloadForUnhappyPath(String payload, String endpoint) {
+        response = request.body(payload).post(endpoint).then().extract().response();
+    }
+
+    @Then("^I should get product response status code for error as (.*)$")
+    public void iShouldGetresponseStatusCodeForForUhappyPath(int status) {
+        Assert.assertEquals(status, response.getStatusCode());
+    }
+
+    @And("^I should get product response with expected error as (.*)$")
+    public void iShouldGetErrorMessageResponseForUhappyPath(String expectedError) {
+        String actualError = response.getBody().path("error");
+        Assert.assertEquals(expectedError, actualError);
     }
 
 
